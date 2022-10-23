92dc36e074c07e2f8265c5e19fd002a8c62e81d6
samratkarak10 <114635510+samratkarak10@users.noreply.github.com>
2022-10-23 18:56:40 +0530

    Create fab.py

---
+++ fab.py
@@ -0,0 +1,23 @@
+# for fabs()
+import math
+
+# initializing integer
+x = -2
+
+# initializing float
+y = -2.00
+
+# printing magnitude and type of output
+# type conversion to float
+print("The magnitude of integer is : ", end ="")
+print(math.fabs(x))
+print("The type of output is : ", end ="")
+print(type(math.fabs(x)))
+
+print("\r")
+
+# printing magnitude and type of output
+print("The magnitude of float is : ", end ="")
+print(math.fabs(y))
+print("The type of output is : ", end ="")
+print(type(math.fabs(y)))
